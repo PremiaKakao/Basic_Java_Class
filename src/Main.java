005a7339c01b052b3f51da6dad9141cf690a999d
Kosta <kosta.jevtic753@gmail.com>
2023-03-14 20:11:31 +0100

    First video, done

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        // Add 2 numbers together 2, 3, 5
+        int a = 2;
+        int b = 3;
+        int c = 5;
+        int Sum = a + b;
+        System.out.println(Sum);
+        // Print what is Sum
+        System.out.println("Sum is "+ Sum);
+    }
+}
